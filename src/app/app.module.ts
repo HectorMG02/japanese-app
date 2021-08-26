2402e7b0fdb84192bc84984524ec6ca5b0033803
hector.matias <hector.matias@equipoiriego.es>
2021-08-26 14:44:40 +0200

    zona de los quizs puesta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuizHiraganaComponent } from './main/pages/quiz-hiragana/quiz-hiragana.component';
-import { QuizKatakanaComponent } from './main/pages/quiz-katakana/quiz-katakana.component';
-import { QuizKanjiComponent } from './main/pages/quiz-kanji/quiz-kanji.component';
 import { SharedModule } from './shared/shared.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { QuizComponent } from './main/quiz/quiz.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizHiraganaComponent,
-    QuizKatakanaComponent,
-    QuizKanjiComponent
+    QuizComponent
   ],
   imports: [
     BrowserModule,
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuizComponent } from './quiz/quiz.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    QuizComponent
+  ],
   imports: [
     CommonModule
   ]
