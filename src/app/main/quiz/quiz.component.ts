4c25f093514c3bf709ade1512cf0397410386080
hector.matias <hector.matias@equipoiriego.es>
2021-08-26 17:16:06 +0200

    quiz de hiragana okey

--- src/app/main/quiz/quiz.component.ts
+++ src/app/main/quiz/quiz.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MainService } from '../service/main.service';
 
-interface Kana {
-  kana: string;
-  romaji: string;
+interface Acertados {
+  tipo: string;
+  value: string
 }
 
 @Component({
@@ -13,157 +15,72 @@ interface Kana {
 })
 export class QuizComponent {
 
-  constructor() { }
+  constructor(public mainService: MainService,
+    private _snackBar: MatSnackBar) { }
 
+  modoQuiz: string = "Estudio";
+  aciertos: number = 0;
+  acertados: Acertados[] = [];
 
-  hiragana: Kana[] = [
-    { kana: 'あ', romaji: 'a' },
-    { kana: 'い', romaji: 'i' },
-    { kana: 'う', romaji: 'u' },
-    { kana: 'え', romaji: 'e' },
-    { kana: 'お', romaji: 'o' },
-    { kana: 'か', romaji: 'ka' },
-    { kana: 'き', romaji: 'ki' },
-    { kana: 'く', romaji: 'ku' },
-    { kana: 'け', romaji: 'ke' },
-    { kana: 'こ', romaji: 'ko' },
-    { kana: 'さ', romaji: 'sa' },
-    { kana: 'し', romaji: 'shi' },
-    { kana: 'す', romaji: 'su' },
-    { kana: 'せ', romaji: 'se' },
-    { kana: 'そ', romaji: 'so' },
-    { kana: 'た', romaji: 'ta' },
-    { kana: 'ち', romaji: 'chi' },
-    { kana: 'つ', romaji: 'tsu' },
-    { kana: 'て', romaji: 'te' },
-    { kana: 'と', romaji: 'to' },
-    { kana: 'な', romaji: 'na' },
-    { kana: 'に', romaji: 'ni' },
-    { kana: 'ぬ', romaji: 'nu' },
-    { kana: 'ね', romaji: 'ne' },
-    { kana: 'の', romaji: 'no' },
-    { kana: 'は', romaji: 'ha' },
-    { kana: 'ひ', romaji: 'hi' },
-    { kana: 'ふ', romaji: 'fu' },
-    { kana: 'へ', romaji: 'he' },
-    { kana: 'ほ', romaji: 'ho' },
-    { kana: 'ま', romaji: 'ma' },
-    { kana: 'み', romaji: 'mi' },
-    { kana: 'む', romaji: 'mu' },
-    { kana: 'め', romaji: 'me' },
-    { kana: 'も', romaji: 'mo' },
-    { kana: 'や', romaji: 'ya' },
-    { kana: 'ゆ', romaji: 'yu' },
-    { kana: 'よ', romaji: 'yo' },
-    { kana: 'ら', romaji: 'ra' },
-    { kana: 'り', romaji: 'ri' },
-    { kana: 'る', romaji: 'ru' },
-    { kana: 'れ', romaji: 're' },
-    { kana: 'ろ', romaji: 'ro' },
-    { kana: 'わ', romaji: 'wa' },
-    { kana: 'を', romaji: 'wo' },
-    { kana: 'ん', romaji: 'n' },
-    { kana: 'が', romaji: 'ga' },
-    { kana: 'ぎ', romaji: 'gi' },
-    { kana: 'ぐ', romaji: 'gu' },
-    { kana: 'げ', romaji: 'ge' },
-    { kana: 'ご', romaji: 'go' },
-    { kana: 'ざ', romaji: 'za' },
-    { kana: 'じ', romaji: 'ji' },
-    { kana: 'ず', romaji: 'zu' },
-    { kana: 'ぜ', romaji: 'ze' },
-    { kana: 'ぞ', romaji: 'zo' },
-    { kana: 'だ', romaji: 'da' },
-    { kana: 'ぢ', romaji: 'ji' },
-    { kana: 'づ', romaji: 'zu' },
-    { kana: 'で', romaji: 'de' },
-    { kana: 'ど', romaji: 'do' },
-    { kana: 'ば', romaji: 'ba' },
-    { kana: 'び', romaji: 'bi' },
-    { kana: 'ぶ', romaji: 'bu' },
-    { kana: 'べ', romaji: 'be' },
-    { kana: 'ぼ', romaji: 'bo' },
-    { kana: 'ぱ', romaji: 'pa' },
-    { kana: 'ぴ', romaji: 'pi' },
-    { kana: 'ぷ', romaji: 'pu' },
-    { kana: 'ぺ', romaji: 'pe' },
-    { kana: 'ぽ', romaji: 'po' },
-  ];
-
-  katakana: Kana[] = [
-    { kana: 'ア', romaji: 'a' },
-    { kana: 'イ', romaji: 'i' },
-    { kana: 'ウ', romaji: 'u' },
-    { kana: 'エ', romaji: 'e' },
-    { kana: 'オ', romaji: 'o' },
-    { kana: 'カ', romaji: 'ka' },
-    { kana: 'キ', romaji: 'ki' },
-    { kana: 'ク', romaji: 'ku' },
-    { kana: 'ケ', romaji: 'ke' },
-    { kana: 'コ', romaji: 'ko' },
-    { kana: 'サ', romaji: 'sa' },
-    { kana: 'シ', romaji: 'shi' },
-    { kana: 'ス', romaji: 'su' },
-    { kana: 'セ', romaji: 'se' },
-    { kana: 'ソ', romaji: 'so' },
-    { kana: 'タ', romaji: 'ta' },
-    { kana: 'チ', romaji: 'chi' },
-    { kana: 'ツ', romaji: 'tsu' },
-    { kana: 'テ', romaji: 'te' },
-    { kana: 'ト', romaji: 'to' },
-    { kana: 'ナ', romaji: 'na' },
-    { kana: 'ニ', romaji: 'ni' },
-    { kana: 'ヌ', romaji: 'nu' },
-    { kana: 'ネ', romaji: 'ne' },
-    { kana: 'ノ', romaji: 'no' },
-    { kana: 'ハ', romaji: 'ha' },
-    { kana: 'ヒ', romaji: 'hi' },
-    { kana: 'フ', romaji: 'fu' },
-    { kana: 'ヘ', romaji: 'he' },
-    { kana: 'ホ', romaji: 'ho' },
-    { kana: 'マ', romaji: 'ma' },
-    { kana: 'ミ', romaji: 'mi' },
-    { kana: 'ム', romaji: 'mu' },
-    { kana: 'メ', romaji: 'me' },
-    { kana: 'モ', romaji: 'mo' },
-    { kana: 'ヤ', romaji: 'ya' },
-    { kana: 'ユ', romaji: 'yu' },
-    { kana: 'ヨ', romaji: 'yo' },
-    { kana: 'ラ', romaji: 'ra' },
-    { kana: 'リ', romaji: 'ri' },
-    { kana: 'ル', romaji: 'ru' },
-    { kana: 'レ', romaji: 're' },
-    { kana: 'ロ', romaji: 'ro' },
-    { kana: 'ワ', romaji: 'wa' },
-    { kana: 'ヲ', romaji: 'wo' },
-    { kana: 'ン', romaji: 'n' },
-    { kana: 'ガ', romaji: 'ga' },
-    { kana: 'ギ', romaji: 'gi' },
-    { kana: 'グ', romaji: 'gu' },
-    { kana: 'ゲ', romaji: 'ge' },
-    { kana: 'ゴ', romaji: 'go' },
-    { kana: 'ザ', romaji: 'za' },
-    { kana: 'ジ', romaji: 'ji' },
-    { kana: 'ズ', romaji: 'zu' },
-    { kana: 'ゼ', romaji: 'ze' },
-    { kana: 'ゾ', romaji: 'zo' },
-    { kana: 'ダ', romaji: 'da' },
-    { kana: 'ヂ', romaji: 'ji' },
-    { kana: 'ヅ', romaji: 'zu' },
-    { kana: 'デ', romaji: 'de' },
-    { kana: 'ド', romaji: 'do' },
-    { kana: 'バ', romaji: 'ba' },
-    { kana: 'ビ', romaji: 'bi' },
-    { kana: 'ブ', romaji: 'bu' },
-    { kana: 'ベ', romaji: 'be' },
-    { kana: 'ボ', romaji: 'bo' },
-    { kana: 'パ', romaji: 'pa' },
-    { kana: 'ピ', romaji: 'pi' },
-    { kana: 'プ', romaji: 'pu' },
-    { kana: 'ペ', romaji: 'pe' },
-    { kana: 'ポ', romaji: 'po' },
-  ];
+  hiragana = this.mainService.hiragana;
+  katakana = this.mainService.katakana;
+  kanji = this.mainService.kanji;
 
 
 
+  changeMode(tipo: string): void {
+    if (tipo == 'Estudio') {
+      this.modoQuiz = 'Estudio';
+
+      // order the hiragana array by the field id
+      this.hiragana = this.hiragana.sort((a, b) => a.id - b.id);
+      this.katakana = this.katakana.sort((a, b) => a.id - b.id);
+
+    } else if (tipo == 'Examen') {
+      this.modoQuiz = 'Examen';
+
+      this.hiragana = this.hiragana.sort(() => {
+        return 0.5 - Math.random();
+      });
+
+      this.katakana = this.katakana.sort(() => {
+        return 0.5 - Math.random();
+      });
+
+    }
+  }
+
+
+  checkRomaji(tipo: string, solution: string, event: any): void {
+    const romaji = event.target.value;
+
+    const existe = this.acertados.filter(item => item.tipo === tipo && item.value === romaji);
+
+
+    if (romaji === solution && !existe.length) {
+      this.aciertos++;
+      this.acertados.push({ tipo: tipo, value: solution });
+      this.openSnackBar('Hacertaste (^^)');
+
+      // quit the focus from the event
+      event.target.blur();
+
+      // set the input as disabled
+      event.target.disabled = true;
+
+      // set the background color to green
+      event.target.style.backgroundColor = '#78f272';
+    } else {
+      event.target.style.backgroundColor = '#fa6c61';
+    }
+  }
+
+
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message);
+    setTimeout(() => {
+      this._snackBar.dismiss();
+    }, 1500);
+  }
 }
